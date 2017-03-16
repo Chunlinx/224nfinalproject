bdd34e33f2a61dd2afc8acdabc4821b1bcae4070
Julian <yg1811@hotmail.com>
2017-03-16 01:23:11 -0700

    modify flags to run on gpu

--- code/train.py
+++ code/train.py
@@ -22,7 +22,7 @@ tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate.")
 tf.app.flags.DEFINE_float("max_gradient_norm", 10.0, "Clip gradients to this norm.")
 tf.app.flags.DEFINE_float("fw_dropout", 0.85, "Fraction of units not randomly dropped on foward non-recurrent connections.")
 tf.app.flags.DEFINE_float("bw_dropout", 0.82, "Fraction of units not randomly dropped on backward non-recurrent connections.")
-tf.app.flags.DEFINE_integer("batch_size", 5, "Batch size to use during training.")  # 32
+tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size to use during training.")  # 32
 tf.app.flags.DEFINE_integer("epochs", 10, "Number of epochs to train.")
 tf.app.flags.DEFINE_integer("state_size", 200, "Size of each model layer.")
 tf.app.flags.DEFINE_integer("output_size", 750, "The output size of your model.")   # 750
@@ -38,7 +38,7 @@ tf.app.flags.DEFINE_integer("keep", 0, "How many checkpoints to keep, 0 indicate
 tf.app.flags.DEFINE_string("vocab_path", "../data/squad/vocab.dat", "Path to vocab file (default: ./data/squad/vocab.dat)")
 tf.app.flags.DEFINE_string("embed_path", "../data/squad/glove.trimmed.100.npz", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{vocab_dim}.npz)")
 tf.app.flags.DEFINE_integer("evaluate", 100, "How many samples to evaluate EM and F1 score.")
-tf.app.flags.DEFINE_integer("test_run", 1, "1 for run on tiny dataset; 0 for full dataset")
+tf.app.flags.DEFINE_integer("test_run", 0, "1 for run on tiny dataset; 0 for full dataset")
 tf.app.flags.DEFINE_integer("baseline", 0, "1 for running baseline model; 0 for MatchLSTM implementation")
 tf.app.flags.DEFINE_string("model", "sequence", "boundary / sequence")
 tf.app.flags.DEFINE_integer("bidirectional_preprocess", 1, "1 for using BiDirect in LSTM Preprocessing layer, 0 for forward only")
